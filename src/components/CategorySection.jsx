d692945bdf0c95778736e1f08d635835c33cd8a8
manasa321 <56963182+manasa321@users.noreply.github.com>
2024-09-24 15:48:53 +0530

    Update CategorySection.jsx

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -16,7 +16,7 @@ const categories = [
     name: 'Mobile', 
     icon: Smartphone,
     brands: [
-      { name: 'Apple', logo: 'https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png' },
+      { name: 'Apple', logo: 'https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png', url:'https://www.apple.com/in/iphone/'},
       { name: 'Samsung', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/2560px-Samsung_Logo.svg.png' },
       { name: 'OnePlus', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKxyMJDTRJRMAOTYgrKR5fslKamX4AUHfbtQ&s' },
       { name: 'Xiaomi', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Xiaomi_logo.svg/2048px-Xiaomi_logo.svg.png' },

68e31ffe9b0fd1e08265042caf26697793cec034
manasa321 <56963182+manasa321@users.noreply.github.com>
2024-09-24 15:50:41 +0530

    Update CategorySection.jsx

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -17,7 +17,7 @@ const categories = [
     icon: Smartphone,
     brands: [
       { name: 'Apple', logo: 'https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png', url:'https://www.apple.com/in/iphone/'},
-      { name: 'Samsung', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/2560px-Samsung_Logo.svg.png' },
+      { name: 'Samsung', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/2560px-Samsung_Logo.svg.png', url:'https://www.samsung.com/in/aisearch/?searchvalue=mobiles' },
       { name: 'OnePlus', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKxyMJDTRJRMAOTYgrKR5fslKamX4AUHfbtQ&s' },
       { name: 'Xiaomi', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Xiaomi_logo.svg/2048px-Xiaomi_logo.svg.png' },
       { name: 'Vivo', logo: 'https://1000logos.net/wp-content/uploads/2022/02/Vivo-Logo.jpg' },

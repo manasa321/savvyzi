4499c30ee36957eef6e23514b8c7383ed0f55f4c
manasa321 <56963182+manasa321@users.noreply.github.com>
2024-09-24 14:51:08 +0530

    Update CategorySection.jsx

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -31,9 +31,9 @@ const categories = [
       { name: 'LG', logo: 'https://images.squarespace-cdn.com/content/v1/502a8efb84ae42cbccf920c4/1585574686746-VCDIHSO21O76WR72WIAD/LG-Logo.png' },
       { name: 'Sony', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_71af1QHWToXD7zD-eRrXfbzq8HuUnQzUWQ&s' },
       { name: 'Samsung', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/2560px-Samsung_Logo.svg.png' },
-      { name: 'Panasonic', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Panasonic_logo.svg/2560px-Panasonic_logo.svg.png' },
+      { name: 'Panasonic', logo: 'https://1000logos.net/wp-content/uploads/2017/04/Color-Panasonic-Logo.jpg' },
       { name: 'Mi TV', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Xiaomi_logo.svg/2048px-Xiaomi_logo.svg.png' },
-      { name: 'TCL', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/TCL_logo.svg/2560px-TCL_logo.svg.png' }
+      { name: 'TCL', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9OUetTLDhz74L9g3GTIK2OjpWviR9eBaclQ&s' }
     ]
   },
   { 

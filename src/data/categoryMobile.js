8404fb03ff152a84d032bd21ddc750aa619bd981
manasa321 <56963182+manasa321@users.noreply.github.com>
2024-10-06 11:31:35 +0530

    Update categoryMobile.js

--- src/data/categoryMobile.js
+++ src/data/categoryMobile.js
@@ -5,5 +5,4 @@ export const mobileBrands = [
   { name: 'Xiaomi', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Xiaomi_logo.svg/2048px-Xiaomi_logo.svg.png', url: 'https://www.mi.com/in/phone' },
   { name: 'Vivo', logo: 'https://1000logos.net/wp-content/uploads/2022/02/Vivo-Logo.jpg', url: 'https://www.vivo.com/in/products/series/y' },
   { name: 'Oppo', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/OPPO_Logo_wiki.png/1024px-OPPO_Logo_wiki.png', url: 'https://www.oppo.com/in/smartphones/' },
-  { name: 'Daily Objects', logo: 'https://images.dailyobjects.com/marche/icons/logo_named.png?tr=cm-pad_resize,v-3,w-135,h-27,dpr-2,q-60', url:'https://track.vcommission.com/click?campaign_id=11432&pub_id=117026' }
 ];

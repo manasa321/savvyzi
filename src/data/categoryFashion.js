58c24681d01774c7dbed302907258af7184e416c
manasa321 <56963182+manasa321@users.noreply.github.com>
2024-10-06 12:30:29 +0530

    Update categoryFashion.js

--- src/data/categoryFashion.js
+++ src/data/categoryFashion.js
@@ -5,14 +5,13 @@ export const fashionSubcategories = [
     brands: [
       { name: 'H&M', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png', url: 'https://www2.hm.com/' },
       { name: 'Zara', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Zara_Logo.svg/2560px-Zara_Logo.svg.png', url: 'https://www.zara.com/' },
-      { name: 'Uniqlo', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UNIQLO_logo.svg/2560px-UNIQLO_logo.svg.png', url: 'https://www.uniqlo.com/' },
       { name: 'Nike', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Logo_NIKE.svg/2560px-Logo_NIKE.svg.png', url: 'https://www.nike.com/' },
       { name: 'Adidas', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/2560px-Adidas_Logo.svg.png', url: 'https://www.adidas.com/' },
       { name: 'Wildcraft', logo: '', url: 'https://track.vcommission.com/click?campaign_id=11277&pub_id=117026' },
       { name: 'Snitch', logo: '', url: 'https://track.vcommission.com/click?campaign_id=10803&pub_id=117026' },
       { name: 'Mufti', logo: '', url: 'https://track.vcommission.com/click?campaign_id=10712&pub_id=117026' },
       { name: 'Blackberrys', logo: '', url: 'https://track.vcommission.com/click?campaign_id=10604&pub_id=117026' },
-      { name: 'UNIQLO', logo: '', url: 'https://track.vcommission.com/click?campaign_id=10258&pub_id=117026' },
+      { name: 'UNIQLO', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UNIQLO_logo.svg/2560px-UNIQLO_logo.svg.png', url: 'https://track.vcommission.com/click?campaign_id=10258&pub_id=117026' },
     ]
   },
   {

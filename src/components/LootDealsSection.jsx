f082499a8d46a6b601bbc70fe7273bc2930ceaa8
manasa321 <56963182+manasa321@users.noreply.github.com>
2024-10-09 12:03:34 +0530

    Update LootDealsSection.jsx

--- src/components/LootDealsSection.jsx
+++ src/components/LootDealsSection.jsx
@@ -4,7 +4,46 @@ import { Button } from "@/components/ui/button";
 
 const LootDealItems = [
   {
-    
+    {
+    id: 1,
+    brand: "Cadbury",
+    discount: "Flat 40% off",
+    link: "https://clnk.in/vr8O",
+    condition: "",
+    image: "https://cdn0.cuelinks.com/merchant/3516/medium/Cadbury.png?1571644680",
+  },
+  {
+    id: 2,
+    brand: "Aroma Magic",
+    earning: "Flat 25% off",
+    link: "https://clnk.in/vrUv",
+    condition: "",
+    image: "https://cdn0.cuelinks.com/merchant/7175/medium/New_Project_-_2024-08-02T171811.156.png?1722599320",
+  },
+  {
+    id: 3,
+    brand: "Compass Guide",
+    earning: "₹5",
+    link: "https://clnk.in/vrEh",
+    condition: "Install & Use Compass twice",
+    image: "https://cdn0.cuelinks.com/merchant/7171/medium/Screenshot_2024-08-01_180523.png?1722515747",
+  },
+  {
+    id: 4,
+    brand: "Magni View",
+    earning: "₹5",
+    link: "https://clnk.in/vrOO",
+    condition: "Install & Use magnification twice",
+    image: "https://cdn0.cuelinks.com/merchant/6808/medium/unnamed_%281%29.jpg?1719313099",
+  },
+  {
+    id: 5,
+    brand: "Puzzle Stack",
+    earning: "₹5",
+    link: "https://clnk.in/vrOR",
+    condition: "Install & Play 2 Consecutive Games",
+    image: "https://cdn0.cuelinks.com/merchant/6810/medium/unnamed_%284%29.jpg?1719314198",
+  },
   },
   // Add other items if needed
 ];

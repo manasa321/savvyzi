59725e56d494bb88569a9523b30703139048e91a
manasa321 <56963182+manasa321@users.noreply.github.com>
2024-10-19 11:45:59 +0530

    Update BusSearch.jsx

--- src/components/BusSearch.jsx
+++ src/components/BusSearch.jsx
@@ -18,10 +18,11 @@ const BusSearch = () => {
 
   return (
     <div className="bg-white p-4 shadow-md rounded-lg">
-      <Button variant="ghost" onClick={() => navigate('/')} className="mb-4">
-        <ArrowLeft className="mr-2" />
-        Back to Home
-      </Button>
+      <div className="flex items-center mb-8">
+          <BackButton />
+          <br></br>
+          <h1 className="text-3xl font-bold text-secondary-foreground capitalize ml-4">{selectedCategory.name}</h1>
+        </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <Input
           placeholder="From"
